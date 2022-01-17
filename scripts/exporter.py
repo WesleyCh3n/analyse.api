51f26e514bc1135fbfd210f0f363c85eca0ff5ce
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-01-17 17:01:19 +0800

    🍁 complete exporter py

--- scripts/exporter.py
+++ scripts/exporter.py
@@ -9,12 +9,15 @@ parser.add_argument("-r",
     type=int,
     nargs='+',
     action='append')
+parser.add_argument("-f", type=str,)
+parser.add_argument("-c", type=str,)
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    cygt = pd.read_csv('../file/csv/2021-09-26-18-36_CyGt_Dr Tsai_1.csv')
-    df = pd.read_csv('../file/csv/2021-09-26-18-36_result_Dr Tsai_1.csv')
+    prefix = "./file/csv/"
+    df = pd.read_csv(prefix+args.f)
+    cygt = pd.read_csv(prefix+ args.c)
     df = df[df.columns.drop(list(df.filter(regex='support')))] # drop by regex
 
     maxMean = pd.concat([
@@ -26,7 +29,7 @@ if __name__ == "__main__":
             for i in range(r[0], r[1])
         ])
         for r in args.r
-    ]).mean().drop(labels=['time']).to_frame(name='max')
+    ]).mean().drop(labels=['time']).to_frame().rename(index=lambda s: s + '_max')
     minMean = pd.concat([
         pd.concat([
             df[df['time'].between(cygt.start[i], cygt.start[i+1])]
@@ -36,6 +39,13 @@ if __name__ == "__main__":
             for i in range(r[0], r[1])
         ])
         for r in args.r
-    ]).mean().drop(labels=['time']).to_frame(name='min')
+    ]).mean().drop(labels=['time']).to_frame().rename(index=lambda s: s + '_min')
 
-    print(pd.concat([maxMean, minMean], axis=1))
+    selection = ' '.join([f'{cygt.start[r[0]]}-{cygt.start[r[1]]}'
+                          for r in args.r])
+
+    concat_df = pd.concat([maxMean, minMean]).sort_index()
+    result = pd.concat([pd.DataFrame({0: selection}, index=['Selection']),
+                        concat_df])
+    result = result.rename(columns={0: args.f})
+    print(result)
