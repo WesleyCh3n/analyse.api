dd0b842459d42fa468f6f705a4cbb5d6f5bf5994
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-02-28 21:38:00 +0800

    🍁 read cycle

--- scripts/exporter.py
+++ scripts/exporter.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-
+"""
+purpose:
+  export selection result
+usage: python3 exporter.py -r cycle.start cycle.start -f <position file> -c <gait result> -s <out dir>
+example: python3 exporter.py -r 1 11 -f tmp/v3.18.10-en-sample-0.csv -c tmp/v3.18.10-en-sample-1.csv -s result
+example output:
+{
+  "ExportFile": "v3.18.10-en-sample-0-result.csv"
+}
+"""
 import argparse
 import json
 import pandas as pd
 
 from pathlib import Path
 
+from module.cycle import createGaitCycleList, createCycleList
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-r",
@@ -20,12 +30,22 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
     df = pd.read_csv(args.f)
-    cygt = pd.read_csv(args.c)
+    dfcy = pd.read_csv(args.c)
     df = df[df.columns.drop(list(df.filter(regex='support')))] # drop by regex
+
+    # HACK: hard code read lt/rt/db file
+    _, dfcy = createGaitCycleList(df, 'double_support')
+    _, dflt = createCycleList(df, 'LT_single_support')
+    _, dfrt = createCycleList(df, 'RT_single_support')
+    _, dfdb = createCycleList(df, 'double_support')
+    print(f'{dfcy=}')
+    print(f'{dflt=}')
+    print(f'{dfrt=}')
+    print(f'{dfdb=}')
 
     maxMean = pd.concat([
         pd.concat([
-            df[df['time'].between(cygt.start[i], cygt.start[i+1])]
+            df[df['time'].between(dfcy.start[i], dfcy.start[i+1])]
                          .max()
                          .to_frame()
                          .T
@@ -35,7 +55,7 @@ if __name__ == "__main__":
     ]).mean().drop(labels=['time']).to_frame().rename(index=lambda s: s + '_max')
     minMean = pd.concat([
         pd.concat([
-            df[df['time'].between(cygt.start[i], cygt.start[i+1])]
+            df[df['time'].between(dfcy.start[i], dfcy.start[i+1])]
                          .min()
                          .to_frame()
                          .T
@@ -44,7 +64,7 @@ if __name__ == "__main__":
         for r in args.r
     ]).mean().drop(labels=['time']).to_frame().rename(index=lambda s: s + '_min')
 
-    selection = ' '.join([f'{cygt.start[r[0]]}-{cygt.start[r[1]]}'
+    selection = ' '.join([f'{dfcy.start[r[0]]}-{dfcy.start[r[1]]}'
                           for r in args.r])
 
     concat_df = pd.concat([maxMean, minMean]).sort_index()
@@ -54,4 +74,4 @@ if __name__ == "__main__":
     result.to_csv(Path(args.s)/f"{Path(args.f).stem}-result.csv")
     print(json.dumps({
         'ExportFile': f"{Path(args.f).stem}-result.csv"
-    }))
+    }, indent=2))
